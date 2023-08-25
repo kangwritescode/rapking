2919d9533fbfb0d015a9dff64eeec0143b3fa7bb
kangwritescode <kangwritescode@gmail.com>
2023-08-25 01:11:09 -0700

    some changes

--- src/pages/write/TextEditor.tsx
+++ src/pages/write/TextEditor.tsx
@@ -26,6 +26,7 @@ export default function TextEditor() {
   return (
     <RichTextEditor
       editor={editor}
+      onChange={(event) => console.log(event)}
       styles={{
         content: {
           background: theme.palette.background.default,
