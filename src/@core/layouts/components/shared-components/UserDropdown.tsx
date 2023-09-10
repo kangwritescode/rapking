286a913ddd745959451569328690f0e4c3cea5f8
David Kang <kangwritescode@gmail.com>
2023-09-09 22:45:04 -0700

    Footer Content

--- src/@core/layouts/components/shared-components/UserDropdown.tsx
+++ src/@core/layouts/components/shared-components/UserDropdown.tsx
@@ -134,12 +134,6 @@ const UserDropdown = (props: Props) => {
         </MenuItem>
         <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
           <Box sx={styles}>
-            <Icon icon='mdi:email-outline' />
-            Inbox
-          </Box>
-        </MenuItem>
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
             <Icon icon='mdi:message-outline' />
             Chat
           </Box>
@@ -149,18 +143,6 @@ const UserDropdown = (props: Props) => {
           <Box sx={styles}>
             <Icon icon='mdi:cog-outline' />
             Settings
-          </Box>
-        </MenuItem>
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
-            <Icon icon='mdi:currency-usd' />
-            Pricing
-          </Box>
-        </MenuItem>
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
-            <Icon icon='mdi:help-circle-outline' />
-            FAQ
           </Box>
         </MenuItem>
         <Divider />
--- src/@core/layouts/components/shared-components/footer/FooterContent.tsx
+++ src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -3,10 +3,7 @@ import Link from 'next/link'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
-import { Theme } from '@mui/material/styles'
 import { styled } from '@mui/material/styles'
-import Typography from '@mui/material/Typography'
-import useMediaQuery from '@mui/material/useMediaQuery'
 
 const LinkStyled = styled(Link)(({ theme }) => ({
   textDecoration: 'none',
@@ -14,40 +11,26 @@ const LinkStyled = styled(Link)(({ theme }) => ({
 }))
 
 const FooterContent = () => {
-  // ** Var
-  const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'))
 
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
-      <Typography sx={{ mr: 2 }}>
-        {`© ${new Date().getFullYear()}, Made with `}
-        <Box component='span' sx={{ color: 'error.main' }}>
-          ❤️
-        </Box>
-        {` by `}
-        <LinkStyled target='_blank' href='https://mui.com/store/contributors/themeselection/'>
-          ThemeSelection
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'flex-end' }}>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
+        <LinkStyled target='_blank' href='https://mui.com/store/license/'>
+          License
         </LinkStyled>
-      </Typography>
-      {hidden ? null : (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
-          <LinkStyled target='_blank' href='https://mui.com/store/license/'>
-            License
-          </LinkStyled>
-          <LinkStyled target='_blank' href='https://mui.com/store/contributors/themeselection/'>
-            More Themes
-          </LinkStyled>
-          <LinkStyled
-            target='_blank'
-            href='https://demos.themeselection.com/marketplace/materio-mui-react-nextjs-admin-template/documentation'
-          >
-            Documentation
-          </LinkStyled>
-          <LinkStyled target='_blank' href='https://themeselection.com/support/'>
-            Support
-          </LinkStyled>
-        </Box>
-      )}
+        <LinkStyled target='_blank' href='https://mui.com/store/contributors/themeselection/'>
+          More Themes
+        </LinkStyled>
+        <LinkStyled
+          target='_blank'
+          href='https://demos.themeselection.com/marketplace/materio-mui-react-nextjs-admin-template/documentation'
+        >
+          Documentation
+        </LinkStyled>
+        <LinkStyled target='_blank' href='https://themeselection.com/support/'>
+          Support
+        </LinkStyled>
+      </Box>
     </Box>
   )
 }
