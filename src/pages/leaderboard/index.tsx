d7658e494c2b20eaedd2c02e68a9e7ddc64d2d4e
David Kang <kangwritescode@gmail.com>
2023-10-09 23:18:40 -0700

    data grid

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import DataGridDemo from './DataGrid';
+import UserLeaderboard from './DataGrid';
 import { Box } from '@mui/material';
 import { api } from 'src/utils/api';
 
 function LeaderboardPage() {
   const { data } = api.leaderboard.getTopUsersByPoints.useQuery({ page: 0 }, { enabled: true })
 
-  console.log(data);
+  const rows = data?.map(({ userData, points }) => ({
+    id: userData?.id || '',
+    username: userData?.username || '',
+    location: userData?.city || '',
+    region: userData?.region || 'EAST',
+    sex: userData?.sex || '',
+    points: points || 0,
+  }));
 
   return (
     <Box
       sx={{
-        width: '70%',
         margin: 'auto',
+        width: '50%',
       }}
     >
-      <DataGridDemo />
+      {rows ?
+        <UserLeaderboard rows={rows} /> :
+        <div>Loading...</div>}
     </Box>
   )
 }
