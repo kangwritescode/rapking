336d5474594861ed1a9aba2d47b09e1dc572db55
David Kang <kangwritescode@gmail.com>
2023-10-19 22:57:49 -0700

    paginate rap comments

--- src/components/RapComments.tsx
+++ src/components/RapComments.tsx
@@ -1,7 +1,10 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useCallback, useEffect, useState } from 'react'
 import RapComment from './RapComment'
-import { Divider } from '@mui/material'
+import { Box, Divider } from '@mui/material'
 import { api } from 'src/utils/api';
+import { RapCommentWithUserData } from 'src/server/api/routers/rapComment';
+import { useInView } from 'react-intersection-observer';
+import FallbackSpinner from 'src/@core/components/spinner';
 
 interface RapCommentsProps {
   sortBy: 'POPULAR' | 'RECENT';
@@ -9,26 +12,71 @@ interface RapCommentsProps {
 }
 
 function RapComments({ sortBy, rapId }: RapCommentsProps) {
-  const { data: rapComments } = api.rapComment.getRapComments.useQuery({
+
+  const { ref, inView } = useInView();
+
+  const [areMoreToLoad, setAreMoreToLoad] = useState(true);
+  const [page, setPage] = useState(0);
+  const [rapComments, setRapComments] = useState<RapCommentWithUserData[]>([]);
+
+  const { refetch } = api.rapComment.getRapComments.useQuery({
     rapId: rapId as string,
     sortBy,
-    page: 0,
-    pageSize: 10,
+    page,
+    pageSize: 6,
   }, {
     enabled: !!rapId,
   });
 
-  return rapComments?.map((comment) =>
-    <Fragment key={comment.id}>
-      <RapComment
-        sx={{
-          py: 5
-        }}
-        comment={comment}
-      />
-      <Divider />
-    </Fragment>
-  )
+  const loadNextPage = useCallback(async () => {
+    const { status, data: rapCommentsData } = await refetch();
+    if (status === 'success') {
+      if (rapCommentsData.length) {
+        setRapComments((prevRaps) => [...prevRaps, ...rapCommentsData]);
+        setPage((prevPage) => prevPage + 1);
+      } else {
+        setAreMoreToLoad(false);
+      }
+    }
+  }, [refetch])
+
+  // Load initial page
+  useEffect(() => {
+    if (!rapComments.length && areMoreToLoad) {
+      loadNextPage();
+    }
+  }, [loadNextPage, rapComments, areMoreToLoad])
+
+  // Load next page when inView
+  useEffect(() => {
+    if (inView && areMoreToLoad) {
+      loadNextPage();
+    }
+  }, [inView, areMoreToLoad, loadNextPage])
+
+  // Reset state when filters change
+  useEffect(() => {
+    setPage(0);
+    setRapComments([]);
+    setAreMoreToLoad(true);
+  }, [sortBy])
+
+  return <>
+    {rapComments?.map((comment) =>
+      <Fragment key={comment.id}>
+        <RapComment
+          sx={{
+            py: 5
+          }}
+          comment={comment}
+        />
+        <Divider />
+      </Fragment>
+    )}
+    <Box ref={ref} height='100px'>
+      {inView && areMoreToLoad && <FallbackSpinner sx={{ height: '100px' }} />}
+    </Box>
+  </>
 }
 
 export default RapComments
