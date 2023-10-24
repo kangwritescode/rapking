b2763b55e6bf42826929808bb397cd0b6caa0f14
David Kang <kangwritescode@gmail.com>
2023-10-24 00:22:32 -0700

    CreateProfileGuard fix

--- src/@core/components/auth/CreateProfileGuard.tsx
+++ src/@core/components/auth/CreateProfileGuard.tsx
@@ -2,34 +2,34 @@ import React, { ReactNode, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import { api } from 'src/utils/api';
+import FallbackSpinner from '../spinner';
 
 interface CreateProfileGuardProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const CreateProfileGuard: React.FC<CreateProfileGuardProps> = ({ children }) => {
-    const { status } = useSession();
-    const router = useRouter();
-    const { data: profileIsComplete, isLoading: isProfileLoading } = api.user.getProfileIsComplete.useQuery(undefined, {
-      enabled: status === 'authenticated',
-    });
+  const { status } = useSession();
+  const router = useRouter();
+  const { data: profileIsComplete, fetchStatus, isLoading } = api.user.getProfileIsComplete.useQuery(undefined, {
+    enabled: status === 'authenticated',
+  });
 
-    // Ensuring both session and profile data are settled
-    const isDataLoading = status === 'loading' || isProfileLoading;
+  const isDataLoading = status === 'loading' || (fetchStatus !== 'idle' && isLoading);
 
-    useEffect(() => {
-        if (isDataLoading) return;
+  useEffect(() => {
+    if (isDataLoading) return;
 
-        if (status === 'authenticated') {
-            if (!profileIsComplete && router.asPath !== '/create-profile/') {
-                router.replace('/create-profile/');
-            } else if (profileIsComplete && router.asPath === '/create-profile/') {
-                router.replace('/');
-            }
-        }
-    }, [status, profileIsComplete, router, isDataLoading]);
+    if (status === 'authenticated') {
+      if (!profileIsComplete && router.asPath !== '/create-profile/') {
+        router.replace('/create-profile/');
+      } else if (profileIsComplete && router.asPath === '/create-profile/') {
+        router.replace('/');
+      }
+    }
+  }, [status, profileIsComplete, router, isDataLoading]);
 
-    return children;
+  return isDataLoading ? <FallbackSpinner /> : children;
 };
 
 export default CreateProfileGuard;
