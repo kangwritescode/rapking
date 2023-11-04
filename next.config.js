af90b9ab45b8f1b67524749344a48e5967450956
David Kang <kangwritescode@gmail.com>
2023-11-04 02:48:06 -0700

    bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const path = require('path');
 
-/** @type {import('next').NextConfig} */
-
-// Remove this if you're not using Fullcalendar features
-
-module.exports = {
+module.exports = withBundleAnalyzer({
   trailingSlash: true,
   reactStrictMode: false,
-  webpack: config => {
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision'),
+    };
 
-    return config
-  }
-}
+    return config;
+  },
+
+});
