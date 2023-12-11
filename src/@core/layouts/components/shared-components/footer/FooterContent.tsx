51ea1672ef4eede080e418b14a9b084c089b15a9
David Kang <kangwritescode@gmail.com>
2023-12-10 22:12:46 -0800

    remove Discord footer link

--- src/@core/layouts/components/shared-components/footer/FooterContent.tsx
+++ src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -1,22 +1,11 @@
-// ** Next Import
-import Link from 'next/link';
-
 // ** MUI Imports
 import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
-
-const LinkStyled = styled(Link)(({ theme }) => ({
-  textDecoration: 'none',
-  color: theme.palette.grey[400]
-}));
 
 const FooterContent = () => {
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'flex-end' }}>
-      <LinkStyled target='_blank' href='https://discord.gg/G5A3N4qm'>
-        Join the Rapking Discord
-      </LinkStyled>
-    </Box>
+    <Box
+      sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'flex-end' }}
+    ></Box>
   );
 };
 
