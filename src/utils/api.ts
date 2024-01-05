5ae43df77160f5f8cedca195e84836fa1755a54e
David Kang <kangwritescode@gmail.com>
2024-01-05 01:06:44 -0800

    loggerLink

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -36,9 +36,7 @@ export const api = createTRPCNext<AppRouter>({
        */
       links: [
         loggerLink({
-          enabled: opts =>
-            process.env.NODE_ENV === 'development' ||
-            (opts.direction === 'down' && opts.result instanceof Error)
+          enabled: () => process.env.NODE_ENV === 'development'
         }),
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`
