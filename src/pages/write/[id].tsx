3c5ec80862bdf1339ead54e6f419d8cbb1c7ebaa
David Kang <57216395+kangwritescode@users.noreply.github.com>
2024-03-10 21:35:20 -0700

    block users from accesing write/[id] for raps they don't own

--- src/pages/write/[id].tsx
+++ src/pages/write/[id].tsx
@@ -1,10 +1,13 @@
 import { Container, useTheme } from '@mui/material';
+import { getSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { GetServerSidePropsContext } from 'next/types';
 import { useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 import sanitize from 'sanitize-html';
 import Footer from 'src/components/Footer';
 import { UpdateRapPayload } from 'src/server/api/routers/rap';
+import { prisma } from 'src/server/db';
 import { api } from 'src/utils/api';
 import RapEditor from '../../components/WritePage/RapEditor';
 
@@ -73,3 +76,38 @@ const ExistingRap = () => {
 };
 
 export default ExistingRap;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context);
+
+  const { id } = context.query;
+
+  if (id) {
+    const rap = await prisma.rap.findUnique({
+      where: {
+        id: id as string
+      }
+    });
+
+    if (!rap) {
+      return {
+        notFound: true
+      };
+    }
+
+    if (session?.user?.id !== rap.userId) {
+      return {
+        redirect: {
+          destination: '/dashboard',
+          permanent: false
+        }
+      };
+    }
+  }
+
+  return {
+    props: {
+      id: id as string
+    }
+  };
+}
