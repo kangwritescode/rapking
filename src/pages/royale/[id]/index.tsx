4f5b31904ed10a920fc2d93ef50003a45cb9d0c4
David Kang <57216395+kangwritescode@users.noreply.github.com>
2024-03-29 19:31:48 -0700

    pst

--- src/pages/royale/[id]/index.tsx
+++ src/pages/royale/[id]/index.tsx
@@ -2,7 +2,6 @@ import { Box, Button, CardMedia, Chip, Divider, Stack, Typography, useTheme } fr
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-import { getMonthAndYear } from 'src/@core/utils/get-formatted-date';
 import Footer from 'src/components/Footer';
 import RoyaleSubmitDialog from 'src/components/RapRoyale/RoyaleSubmitDialog';
 import SubmissionsDataGrid from 'src/components/RapRoyale/SubmissionsDataGrid';
@@ -70,7 +69,8 @@ function RoyalePage() {
           >
             <span>
               <strong>Competition Period:</strong>{' '}
-              {getMonthAndYear(royaleData?.endDate || new Date())}
+              {royaleData?.startDate.toLocaleDateString() || new Date().toLocaleDateString()} -{' '}
+              {royaleData?.endDate.toLocaleDateString() || new Date().toLocaleDateString()} (PST)
             </span>
             <span>
               Status:{' '}
