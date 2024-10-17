b6a10c0aee42079b8eb66285a9ed9d6dbbc6b3bd
David Kang <57216395+kangwritescode@users.noreply.github.com>
2024-10-17 01:42:52 -0700

    RIP RAPKING

--- src/components/UnderConstructionGuard.tsx
+++ src/components/UnderConstructionGuard.tsx
@@ -12,10 +12,7 @@ function UnderConstructionGuard({ children }: UnderConstructionGuardProps) {
       <Stack height='100vh' width='100vw' alignItems='center' justifyContent='center'>
         <Icon icon='tabler:crown' color={theme.palette.secondary.main} height={60} width={60} />
         <Typography variant='h5' fontWeight='600' mt={2}>
-          RapKing is under construction
-        </Typography>
-        <Typography variant='subtitle1' color='text.secondary' mt={2}>
-          Stay tuned
+          RIP RAPKING
         </Typography>
       </Stack>
     );
